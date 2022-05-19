6fb78d59e5b58349e36ffc9857dd620dfeb35615
Alexander Widua <a.widua@gmail.com>
2022-05-19 20:21:21 +0200

    init

---
+++ src/ui/ui.tsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+import './base.css'
+
+function UI() {
+	return <div>Hello world</div>
+}
+
+ReactDOM.render(<UI />, document.getElementById('ui'))
