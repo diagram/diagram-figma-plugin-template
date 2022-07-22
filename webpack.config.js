20ba6fce28f58061dcf7fa71dbc7ecca54c09e79
Andrew Pouliot <andrew@darknoon.com>
2022-07-22 12:37:09 -0400

    run prettier

--- webpack.config.js
+++ webpack.config.js
@@ -6,56 +6,56 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = (env, argv) => {
-	const mode = argv.mode === 'production' ? 'production' : 'development'
-	const isDevMode = mode == 'development'
+    const mode = argv.mode === 'production' ? 'production' : 'development'
+    const isDevMode = mode == 'development'
 
-	return {
-		mode,
-		devtool: argv.mode === 'production' ? false : 'inline-source-map',
-		entry: {
-			ui: './src/ui/ui.tsx',
-			code: './src/plugin/code.ts'
-		},
-		module: {
-			rules: [
-				{
-					test: /\.tsx?$/,
-					use: 'ts-loader',
-					exclude: /node_modules/
-				},
-				{
-					test: /\.css$/,
-					use: ['style-loader', 'css-loader']
-				},
-				{
-					test: /\.svg/,
-					type: 'asset/inline'
-				}
-			]
-		},
-		resolve: { extensions: ['.tsx', '.ts', '.jsx', '.js'] },
-		output: {
-			filename: '[name].js',
-			path: path.resolve(__dirname, 'dist')
-		},
-		optimization: isDevMode
-			? {}
-			: {
-					minimize: true,
-					minimizer: [new TerserPlugin()]
-			  },
-		plugins: [
-			new webpack.DefinePlugin({
-				global: {} // fix missing symbol error when running in developer VM
-			}),
-			new HtmlWebpackPlugin({
-				inject: 'body',
-				template: './template/template.html',
-				filename: 'ui.html',
-				chunks: ['ui'],
-				cache: false
-			}),
-			new InlineChunkHtmlPlugin(HtmlWebpackPlugin, [/ui/])
-		]
-	}
+    return {
+        mode,
+        devtool: argv.mode === 'production' ? false : 'inline-source-map',
+        entry: {
+            ui: './src/ui/ui.tsx',
+            code: './src/plugin/code.ts',
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader'],
+                },
+                {
+                    test: /\.svg/,
+                    type: 'asset/inline',
+                },
+            ],
+        },
+        resolve: { extensions: ['.tsx', '.ts', '.jsx', '.js'] },
+        output: {
+            filename: '[name].js',
+            path: path.resolve(__dirname, 'dist'),
+        },
+        optimization: isDevMode
+            ? {}
+            : {
+                  minimize: true,
+                  minimizer: [new TerserPlugin()],
+              },
+        plugins: [
+            new webpack.DefinePlugin({
+                global: {}, // fix missing symbol error when running in developer VM
+            }),
+            new HtmlWebpackPlugin({
+                inject: 'body',
+                template: './template/template.html',
+                filename: 'ui.html',
+                chunks: ['ui'],
+                cache: false,
+            }),
+            new InlineChunkHtmlPlugin(HtmlWebpackPlugin, [/ui/]),
+        ],
+    }
 }
